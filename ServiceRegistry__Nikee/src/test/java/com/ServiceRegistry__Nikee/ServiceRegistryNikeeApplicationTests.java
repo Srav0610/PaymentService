484c1caca847a1834aab95bbff0586e5ed7c2c40
sravanthi <sravanthimanchikatla06@gmail.com>
2025-06-30 11:59:31 +0530

    intial commit

---
+++ ServiceRegistry__Nikee/src/test/java/com/ServiceRegistry__Nikee/ServiceRegistryNikeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ServiceRegistry__Nikee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryNikeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
